1b7aa90c7cf2e77f6700c4812748f22931c49ad5
Awsder BBM5 <awsd.andercen.022@gmail.com>
2023-05-10 05:40:47 +0300

    Добавьте файлы проекта.

---
+++ PrInf_lab4_client/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PrInf_lab4_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(157, 343);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 69);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(36, 30);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(512, 295);
+            textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(981, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+    }
+}
---
+++ PrInf_lab4_client/Form1.cs
@@ -0,0 +1,148 @@
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using System.Diagnostics;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace PrInf_lab4_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs eventArg)
+        {
+            BigInteger message = 555;
+            // Генерация закрытого ключа
+            string privateKeyPath = $"privatekey.pem";
+            string publicKeyPath = $"publickey.pem";
+            string privateKeyCommand = $"genpkey -algorithm RSA -out {privateKeyPath} -pkeyopt rsa_keygen_bits:1024";
+            ExecuteOpenSSLCommand(privateKeyCommand);
+
+            // Извлечение открытого ключа из закрытого
+            string publicKeyCommand = $"rsa -pubout -in {privateKeyPath} -out {publicKeyPath}";
+            ExecuteOpenSSLCommand(publicKeyCommand);
+
+            // Чтение открытого ключа из файла
+            string publicKeyContents = File.ReadAllText(publicKeyPath);
+            // Парсинг открытого ключа в PEM формате
+            PemReader reader = new PemReader(new StringReader(publicKeyContents));
+            RsaKeyParameters publicKeyParams = (RsaKeyParameters)reader.ReadObject();
+            // Извлечение значения e (открытая экспонента)
+            BigInteger e = new BigInteger(publicKeyParams.Exponent.ToByteArrayUnsigned());
+            textBox1.Text += "Экспонента = " + e.ToString() + Environment.NewLine;
+            // Извлечение значения n (модуль)
+            BigInteger n = new BigInteger(publicKeyParams.Modulus.ToByteArrayUnsigned());
+            textBox1.Text += "Модуль = " + n.ToString() + Environment.NewLine;
+
+            // Генерация случайного затемняющего множителя
+            BigInteger r = GenerateRandomNumber(n);
+            // Умножение сообщения на затемняющий множитель
+            BigInteger blindedMessage = message * r % n;
+            textBox1.Text += "Склеенное сообщение = " + blindedMessage + Environment.NewLine;
+
+            // Чтение закрытого ключа из файла
+            string privateKeyContents = File.ReadAllText(privateKeyPath);
+            // Парсинг закрытого ключа в PEM формате
+            PemReader reader2 = new PemReader(new StringReader(privateKeyContents));
+            RsaPrivateCrtKeyParameters privateKeyParams = (RsaPrivateCrtKeyParameters)reader2.ReadObject();
+            // Извлечение значения d (закрытая экспонента)
+            BigInteger privateKey_d = new BigInteger(privateKeyParams.Exponent.ToByteArrayUnsigned());
+            // Подписание склеенного сообщения
+            BigInteger blindedSignature = ModPow(blindedMessage, privateKey_d, n);
+            textBox1.Text += "Подписанное сообщение = " + blindedSignature + Environment.NewLine;
+
+            // Снимание затемняющего множителя
+            BigInteger unblindedSignature = ModInverse(r, n) * blindedSignature % n;
+            textBox1.Text += "Раскрытое сообщение = " + unblindedSignature + Environment.NewLine;
+
+            // Сохранение сообщения и подписи в файлы
+            string messageFilePath = "message.txt";
+            File.WriteAllText(messageFilePath, message.ToString());
+            string signatureFilePath = "signature.bin";
+            File.WriteAllBytes(signatureFilePath, unblindedSignature.ToByteArray());
+
+            // Проверка подписи с использованием OpenSSL
+            string opensslCommand = $"dgst -sha256 -verify {publicKeyPath} -signature {signatureFilePath} {messageFilePath}";
+            ExecuteOpenSSLCommand(opensslCommand);
+
+        }
+        private BigInteger ModPow(BigInteger baseValue, BigInteger exponent, BigInteger modulus)
+        {
+            BigInteger result = 1;
+            while (exponent > 0)
+            {
+                if (exponent % 2 == 1)
+                    result = (result * baseValue) % modulus;
+                baseValue = (baseValue * baseValue) % modulus;
+                exponent /= 2;
+            }
+            return result;
+        }
+
+        private void ExecuteOpenSSLCommand(string arguments)
+        {
+            string command = "openssl";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = command;
+            startInfo.Arguments = arguments;
+            startInfo.RedirectStandardOutput = true;
+
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            textBox1.Text += output + Environment.NewLine;
+            process.WaitForExit();
+        }
+
+        public BigInteger GenerateRandomNumber(BigInteger n)
+        {
+            byte[] bytes = n.ToByteArray();
+            BigInteger randomNumber;
+
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                do
+                {
+                    rng.GetBytes(bytes);
+                    randomNumber = new BigInteger(bytes);
+                } while (randomNumber >= n);
+            }
+
+            return randomNumber;
+        }
+
+        public BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger m0 = n;
+            BigInteger y = 0, x = 1;
+
+            if (n == 1)
+                return 0;
+
+            while (a > 1)
+            {
+                BigInteger q = a / n;
+                BigInteger t = n;
+
+                n = a % n;
+                a = t;
+                t = y;
+
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0)
+                x += m0;
+
+            return x;
+        }
+
+    }
+}
